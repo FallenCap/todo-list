da3472965a15b53f93e13014c1a2c1f262f81c05
FallenCap <fallencap18@gmail.com>
2023-07-02 00:24:08 +0530

    Some update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
-import Signup from './components/SIgnup';
+import Signup from './components/Signup';
 import Login from './components/Login';
 import Profile from './components/Profile';
 
