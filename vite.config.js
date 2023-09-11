0133d7e2000d356f43749d7a3d406ecc482870f4
FallenCap <fallencap18@gmail.com>
2023-09-11 11:27:14 +0530

    adding vite pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'TODO-List',
+        short_name: 'TODO-List',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ff0000',
+        lang: 'en',
+        scope: '/',
+        icons: [
+          {
+            src: '../public/logo192.png',
+            sizes: '48x48 72x72 96x96 128x128 256x256 512x512',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          },
+        ],
+        theme_color: '#242424',
+      },
+    }),
+  ],
 });
